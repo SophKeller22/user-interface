c27b5d7a95f59f7810562da386b0dcdb4213fdb5
Sophia Keller <sophiakeller22@gmail.com>
2022-10-06 22:04:03 -0400

    Added map, popup, and started creating data

--- project1/js/main.js
+++ project1/js/main.js
@@ -1,7 +1,5 @@
 // Javascript for Project 1: Smart Shopping Cart Interface
-
-// Array to hold all items in cart
-let cart = [];
+console.log("hello");
 
 function addToCart() {
     // TODO
@@ -14,3 +12,18 @@ function calcPrice() {
 function initializeUi() {
     // Function to prefill cart so that certain elements in display are visible
 }
+
+// When user clicks on help button, open popup
+function showHelpPopup() {
+    document.getElementById("help_popup").style.visibility= "visible";
+}
+
+// When user clicks ok button, hide popup
+function hideHelpPopup() {
+    document.getElementById("help_popup").style.visibility= "hidden";
+}
+
+// Array to hold all items in cart
+let cart = [];
+
+console.log(window.location.pathname);
